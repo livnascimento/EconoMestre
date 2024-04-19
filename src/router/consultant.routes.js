0ae533e5dcdcf8721d16be3799f7ffbdba6ad16c
Lívia <livia.do.nascimento@avanade.com>
2024-04-19 13:52:19 -0300

    feat: rotas de Consultant

--- src/router/consultant.routes.js
+++ src/router/consultant.routes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { createConsultant, deleteConsultant, getConsultantById, getConsultants, updateConsultant } from "../controllers/consultant.controller.js";
 
 const consultantRouter = express();
+
+consultantRouter.post("/consultant", createConsultant);
+consultantRouter.get("/consultant/:id", getConsultantById);
+consultantRouter.get("/consultant", getConsultants);
+consultantRouter.put("/consultant/:id", updateConsultant);
+consultantRouter.delete("/consultant/:id", deleteConsultant);
 
 export default consultantRouter;
