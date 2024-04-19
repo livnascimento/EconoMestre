bf844d43666a8162cc1ae7b4bc95ef6e987f56b9
Lívia <livia.do.nascimento@avanade.com>
2024-04-19 13:51:33 -0300

    feat: class Client

---
+++ src/classes/Client.js
@@ -0,0 +1,10 @@
+class Client {
+    constructor(name, cpf, email, password) {
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+export default Client;
