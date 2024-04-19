cae71de1417c1a846eb1d47f4ab453440105c8e4
Lívia <livia.do.nascimento@avanade.com>
2024-04-19 13:54:35 -0300

    feat: rotas de Client

--- src/router/client.routes.js
+++ src/router/client.routes.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { createClient, deleteClient, getClientById, getClients, updateClient } from "../controllers/client.controller";
 
 const clientRouter = express();
+
+clientRouter.post("/client", createClient);
+clientRouter.get("/client/:id", getClientById);
+clientRouter.get("/client", getClients);
+clientRouter.put("/client/:id", updateClient);
+clientRouter.delete("/client/:id", deleteClient);
 
 export default clientRouter;
