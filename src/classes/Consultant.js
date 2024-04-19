e06d769e993c1b706e62f2e10a89bdcbc590dd04
Lívia <livia.do.nascimento@avanade.com>
2024-04-19 13:51:47 -0300

    feat: class Consultant

---
+++ src/classes/Consultant.js
@@ -0,0 +1,11 @@
+class Consultant {
+    constructor(id, name, cnpi, email, password) {
+        this.id = id;
+        this.name = name;
+        this.cnpi = cnpi;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+export default Consultant;
