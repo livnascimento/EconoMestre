0cf9c9aef38183bc504a7fd48eb0b56551576855
Lívia <livia.do.nascimento@avanade.com>
2024-04-19 15:05:15 -0300

    feat: classe Meeting

---
+++ src/classes/Meeting.js
@@ -0,0 +1,10 @@
+class Meeting {
+    constructor(id, id_consultant, id_client, date) {
+        this.id = id;
+        this.id_consultant = id_consultant;
+        this.id_client = id_client;
+        this.date = date;
+    }
+}
+
+export default Meeting;
